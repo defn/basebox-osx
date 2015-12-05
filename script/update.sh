609d828341be37b71febbbf09d74d0e5589e23ac
Tom Bombabil <amanibhavam@destructuring.org>
2015-12-04 23:45:16 -0800

    rebooting causes guest to reinstall from dvd

--- script/update.sh
+++ script/update.sh
@@ -4,8 +4,4 @@ if [[ "$UPDATE" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
 
     echo "==> Running software update"
     softwareupdate --install --all -v
-
-    echo "==> Rebooting the machine"
-    reboot
-
 fi
