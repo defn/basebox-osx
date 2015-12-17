588c4691ac1d95e1bb3101f8df45b6be2cf0745b
Tom Bombabil <amanibhavam@destructuring.org>
2015-12-16 18:54:36 -0800

    cant stop paging while sip in effect

--- script/minimize.sh
+++ script/minimize.sh
@@ -6,10 +6,8 @@ pmset hibernatemode 0
 echo "==> Getting rid of the sleepimage"
 rm -f /var/vm/sleepimage
 
-echo "==> Stopping the page process and dropping swap files"
+echo "==> Dropping swap files"
 # These will be re-created on boot
-launchctl unload /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
-sleep 5
 rm -rf /private/var/vm/swap*
 
 # VMware Fusion specific items
