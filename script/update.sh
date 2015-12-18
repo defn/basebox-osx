c3a4649bd3a0ed139583d9e7cc127b1a3d195959
Tom Bombabil <amanibhavam@destructuring.org>
2015-12-17 21:13:40 -0800

    loop until updates are done

--- script/update.sh
+++ script/update.sh
@@ -3,7 +3,7 @@
 if [[ "$UPDATE" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
 
     echo "==> Running software update"
-    softwareupdate --install --all -v
-    softwareupdate --install --all -v
-    softwareupdate --install --all -v
+    while true; do 
+      softwareupdate --install --all -v && break
+    done
 fi
