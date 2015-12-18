30943ffd412d6d743b4a0dd5db3859e97585358e
Tom Bombabil <amanibhavam@destructuring.org>
2015-12-17 19:27:00 -0800

    updates layer

--- script/update.sh
+++ script/update.sh
@@ -4,4 +4,6 @@ if [[ "$UPDATE" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
 
     echo "==> Running software update"
     softwareupdate --install --all -v
+    softwareupdate --install --all -v
+    softwareupdate --install --all -v
 fi
