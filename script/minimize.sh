4ea453c35d7f306f9f23c17fb727adbf11bf4ca5
Tom Bombabil <amanibhavam@destructuring.org>
2016-01-09 17:44:17 -0800

    fixed from upstream

--- script/minimize.sh
+++ script/minimize.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -eux
 
-echo "==> Turning off hibernation"
+echo "==> Turn off hibernation"
 pmset hibernatemode 0
 
-echo "==> Getting rid of the sleepimage"
+echo "==> Get rid of the sleepimage"
 rm -f /var/vm/sleepimage
 
-echo "==> Dropping swap files"
+echo "==> Stop the page process and dropping swap files"
 # These will be re-created on boot
+if csrutil status | grep -q disabled; then
+  launchctl unload /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
+  sleep 5
+fi
 rm -rf /private/var/vm/swap*
 
 # VMware Fusion specific items
 if [ -e .vmfusion_version ] || [[ "$PACKER_BUILDER_TYPE" == vmware* ]]; then
     # Shrink the disk
-    sudo /Library/Application\ Support/VMware\ Tools/vmware-tools-cli disk shrink /
+    /Library/Application\ Support/VMware\ Tools/vmware-tools-cli disk shrink /
 fi
