2cadb2428953f3c43cd53f111a39c5c68e5d0651
Tom Bombabil <amanibhavam@destructuring.org>
2016-01-09 20:48:24 -0800

    try killing softwareupdate daemon

--- script/xcode.sh
+++ script/xcode.sh
@@ -6,5 +6,6 @@ touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 # find the CLI Tools update
 PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
 # install it
+killall softwareupdated
 softwareupdate -i "$PROD" -v
 rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
