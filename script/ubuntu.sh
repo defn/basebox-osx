0c1f846db5026040084d788f9ac350836a9be0eb
Tom Bombabil <amanibhavam@destructuring.org>
2016-01-16 17:16:24 -0800

    made osx scripts look like ubuntu

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -11,12 +11,9 @@ scutil --set HostName ubuntu.dev
 
 # Packer passes boolean user variables through as '1', but this might change in
 # the future, so also check for 'true'.
-if [ "$INSTALL_VAGRANT_KEYS" = "true" ] || [ "$INSTALL_VAGRANT_KEYS" = "1" ]; then
-	echo "Installing vagrant keys for $SSH_USERNAME user"
-	mkdir "/Users/$SSH_USERNAME/.ssh"
-	chmod 700 "/Users/$SSH_USERNAME/.ssh"
-	curl -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > "/Users/$SSH_USERNAME/.ssh/authorized_keys"
-	chmod 600 "/Users/$SSH_USERNAME/.ssh/authorized_keys"
-	chown -R "$SSH_USERNAME" "/Users/$SSH_USERNAME/.ssh"
-fi
-
+echo "Installing vagrant keys for $SSH_USERNAME user"
+mkdir "/Users/$SSH_USERNAME/.ssh"
+chmod 700 "/Users/$SSH_USERNAME/.ssh"
+curl -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > "/Users/$SSH_USERNAME/.ssh/authorized_keys"
+chmod 600 "/Users/$SSH_USERNAME/.ssh/authorized_keys"
+chown -R "$SSH_USERNAME" "/Users/$SSH_USERNAME/.ssh"
--- script/update.sh
+++ script/update.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -eux
 
-if [[ "$UPDATE" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
-
-    echo "==> Running software update"
-    while true; do 
-      softwareupdate --install --all -v && break
-    done
-fi
+echo "==> Running software update"
+softwareupdate --install --all -v
