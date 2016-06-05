234d39e51380c25c21ed02b7058df093df1f63fc
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-05 14:02:02 -0700

    use shome hints

--- script/sshd.sh
+++ script/sshd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function main {
-  local shome="$(cd -P -- "${BASH_SOURCE%/*}/.." && pwd -P)"
+  local shome="${_basebox_osx_home:-"$(cd -P -- "${BASH_SOURCE%/*}/.." && pwd -P)"}"
 }
 
 main "$@"
